449f49d7a5c09201c51b789d3d6dee459685401f
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-06 12:56:48 +0600

    dark theme added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('.nav-links');
+
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector(".nav-links");
 
 menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
+  navLinks.classList.toggle("active");
+};
+
+const darkTheme = document.getElementById("dark-theme");
+const body = document.body;
+
+darkTheme.onclick = () => {
+  body.classList.toggle("dark-theme"); // Toggle dark-theme class
+};
